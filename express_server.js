d9d1fbf115d59b3b704d26618ff11c8e00869c29
eliachow <eliacchow@gmail.com>
2023-03-03 09:10:29 -0800

    eslint express_server file and add ejs to JSON packages

--- express_server.js
+++ express_server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 
 const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
-    res.send("Hello!");
+  res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -21,6 +21,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b><body></html>\n");
 });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
