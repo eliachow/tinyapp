6769842b59369c66f183b26ea0ad79fb1216b7aa
eliachow <eliacchow@gmail.com>
2023-03-04 14:39:34 -0800

    create a post route from urls_show to edit the longURL with the new inputted longURL and redirect to the urls_index page

--- express_server.js
+++ express_server.js
@@ -15,18 +15,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+//renders hello on root page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b><body></html>\n");
-});
+// //renders Hello World on hello page
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b><body></html>\n");
+// });
 
+//renders table from urls_index template on /urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -37,27 +42,38 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-
+//creates random shortURL ID for input long URL
+//redirects to new shortURL ID edi page
 app.post("/urls", (req, res) => {
   const newURLID = generateRandomString();
   urlDatabase[newURLID] = req.body.longURL;
   res.redirect(`/urls/${newURLID}`);
 });
 
-
+//redirects to the shortURL ID's longURL
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
-
+//renders shortURL ID edit page
 app.get("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
   const templateVars = { id: shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 });
 
+//ocurring from URL edit && URL ID
+app.post("/urls/:id", (req, res) => {
+  console.log("👉👉👉", req.body);
+  const shortURL = req.params.id;
+  // const longURL =
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});
+
+//when the delete button is submitted the URL is deleted
 app.post("/urls/:id/delete", (req, res) => {
   const shortURL = req.params.id;
   delete urlDatabase[shortURL];
